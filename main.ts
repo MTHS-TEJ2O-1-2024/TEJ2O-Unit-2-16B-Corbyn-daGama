bd91e424555f10416cc2802e88f0f023b472766f
Corbyn-daGama <corbyn.da-gama@mths.ca>
2024-11-15 11:23:19 -0500

    This program knows when a micro:bit is too close to one another

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Corbyn da Gama
+ * Created on: Nov 2024
+ * This program knows when a micro:bit is too close to one another
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(251)
+basic.showIcon(IconNames.Happy)
+
+// variable
+let distanceToObject: number 
+
+// find distance from sonar
+input.onButtonPressed(Button.A, function () {
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+)
+
+    // if distance is <= 10
+    if (distanceToObject <= 10) {
+    radio.sendString("Too Close")
+}
+})
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
